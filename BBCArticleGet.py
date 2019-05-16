71939e650971b7a3b446afc550a14645797542f2
IamCathal <cathalsocallaghan@gmail.com>
2019-05-16 17:43:44 +0100

    Update BBCArticleGet.py

--- BBCArticleGet.py
+++ BBCArticleGet.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib.error import HTTPError
 import datetime
 import time
 import csv
@@ -20,13 +21,13 @@ def getTitle(dir):
         return None
     else:
         title = htmlObj.title.text
-        print(title)
+        print('Downloaded articles from '+title)
 
 def getArticleTitles(dir):
         html = urlopen('https://www.bbc.com/news/'+dir)
         htmlObj = BeautifulSoup(html, features="html.parser")
 
-        articles = htmlObj.findAll("span", "title-link__title-text")
+        articles = htmlObj.findAll("span", re.compile("(title-link__title-text)+"))
         return articles
 
 def getArticleTopics(dir):
@@ -36,12 +37,12 @@ def getArticleTopics(dir):
         articles = htmlObj.findAll("li", "mini-info-list__item mini-info-list__item--section")
         #for elem in articles:
         #    print(elem)
-        for elem in articles:
+        for elem in articles: 
             articlesText.append(elem.text[16:]) 
         return articlesText
 
 def writeCSV(allArticles, allTopics, numArticles, dir):
-    with open('info.csv', 'a') as file:
+    with open('info.csv', 'a', encoding="utf-8") as file:
         fields = ['date', 'dir', 'articleTitle','articleTopic']
         writeObj = csv.DictWriter(file, fieldnames=fields,lineterminator='\n')
 
@@ -51,11 +52,11 @@ def writeCSV(allArticles, allTopics, numArticles, dir):
 
 def getInfo(dir):
 
-    getTitle(dir)
+    articleCount = 0
 
+    getTitle(dir)
     articleTitles = getArticleTitles(dir)
     articleTopics = getArticleTopics(dir)
-    articleCount = 0
     for article in articleTitles:
         articleCount += 1
 
@@ -64,24 +65,37 @@ def getInfo(dir):
 def checkLink(url):
     try:
         html = urlopen('https://www.bbc.com/news/'+url)
-    except HTTPerror as err:
+    except HTTPError as err:
         return None
+        exit()
     else: getInfo(url)
 
+def main():
+    getInfo('england')
+    time.sleep(5)
+    getInfo('northern_ireland')
+    time.sleep(5)
+    getInfo('scotland')
+    time.sleep(5)
+    getInfo('wales')
+    time.sleep(5)
+    getInfo('politics')
+    time.sleep(5)
+    # Brexit
+    getInfo('politics/uk_leaves_the_eu')
+    time.sleep(5)
+    getInfo('technology')
+    time.sleep(5)
+    # Global Trade
+    getInfo('business-38507481')
+    time.sleep(5)
+    # Connected World
+    getInfo('business-45489065')
+    time.sleep(5)
+    
+    print("Done!")
 
-getInfo('england')
-time.sleep(5)
-getInfo('northern_ireland')
-time.sleep(5)
-getInfo('scotland')
-time.sleep(5)
-getInfo('wales')
-time.sleep(5)
-getInfo('politics')
-time.sleep(5)
-getInfo('politics/uk_leaves_the_eu')
-time.sleep(5)
-getInfo('technology')
-time.sleep(5)
-print("Done!")
+if __name__ == "__main__":
+    main()
+
 
