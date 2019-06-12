8186cde43b86734fe93089c6573bd3a6dc8e803d
IamCathal <cathalsocallaghan@gmail.com>
2019-06-13 00:50:24 +0100

    Now scrapes XML feeds
    Scraping the main pages broke. Spider now scrapes from XML RSS Feeds which is far more efficient and the code is more compact.

--- BBCArticleGet.py
+++ BBCArticleGet.py
@@ -1,81 +1,56 @@
-from bs4 import BeautifulSoup
-from urllib.request import urlopen
-from urllib.error import HTTPError
+import xml.etree.ElementTree as ET  
+import urllib.request
+import threading
 import datetime
+import random
 import time
 import csv
-import re
 
 date = datetime.datetime.now()
 currDate = '{}/{}/{}'.format(date.day,date.month,date.year)
+random.seed(datetime.datetime.now())
 
-def getTitle(dir):
-    try:
-        html = urlopen('http://feeds.bbci.co.uk/news/'+dir+'/rss.xml')
-    except HTTPError as err:
-        print(err)
-        return None
-    try:
-        htmlObj = BeautifulSoup(html, features="html.parser")
-    except AttributeError as err:
-        return None
-    else:
-        title = htmlObj.title.text
-        print('Downloaded articles from '+title)
+articleURLs = ('world','uk','business','politics','health',
+'education','science_and_environment','technology','entertainmant_and_arts',
+'world/africa','world/asia','world/europe','world/latin_america','world/middle_east',
+'world/us_and_canada','world/england','northern_ireland','scotland','wales')
 
 
-def getArticleTitles(dir):
-        html = urlopen('http://feeds.bbci.co.uk/news/'+dir+'/rss.xml')
-        htmlObj = BeautifulSoup(html, features="html.parser")
+def getArticles(dir):
+    tree = ET.parse(source=urllib.request.urlopen('http://feeds.bbci.co.uk/news/'+dir+'/rss.xml'))
+    root = tree.getroot()
+    invalidArticle = 'BBC News - '+dir
+    allArticles = list()
 
-        articles = list()
-        articles = htmlObj.findAll("li")
-        return articles
-      
+    for elem in root.iter('title'):
+        if elem.text.lower() != invalidArticle.lower():
+                allArticles.append(elem.text)
 
-def writeCSV(allArticles, dir):
-    with open('infocheck.csv', 'a', encoding="utf-8") as file:
+    return allArticles
+
+
+def writeCSV(articleList, dir):
+    with open('infoXML.csv', 'a', encoding="utf-8") as file:
         fields = ['date', 'dir', 'articleTitle']
         writeObj = csv.DictWriter(file, fieldnames=fields,lineterminator='\n')
 
-        for article in allArticles:
+        for article in articleList:
             writeObj.writerow({'date':'{}'.format(currDate),'dir':'{}'.format(dir),'articleTitle':'{}'.format(article)})
 
 
-def getInfo(dir):
-
-    getTitle(dir)
-    
-    articleTitles = getArticleTitles(dir)
-
-    writeCSV(articleTitles, dir)
+def scrape(dir):
+    allArticles = getArticles('world')
+    writeCSV(allArticles, dir)
+    print("Downloaded articles from section: {}".format(dir))
 
 
 def main():
-    getInfo('england')
-    time.sleep(5)
-    getInfo('northern_ireland')
-    time.sleep(5)
-    getInfo('scotland')
-    time.sleep(5)
-    getInfo('wales')
-    time.sleep(5)
-    getInfo('politics')
-    time.sleep(5)
-    # Brexit
-    getInfo('politics/uk_leaves_the_eu')
-    time.sleep(5)
-    getInfo('technology')
-    time.sleep(5)
-    # Global Trade
-    getInfo('business-38507481')
-    time.sleep(5)
-    # Connected World
-    getInfo('business-45489065')
-    
-    print("Done!")
+    for target in articleURLs:
+        scrape(target)
+        waitTime = (random.random())*3
+        # print("Waited {:0.2f} seconds".format(waitTime))
+        time.sleep(waitTime)
 
-if __name__ == "__main__":
+   
+if __name__ == '__main__':
     main()
-
-
