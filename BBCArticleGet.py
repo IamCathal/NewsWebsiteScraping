97fab4a182ea20a7f0111572831380b504e2e30d
IamCathal <cathalsocallaghan@gmail.com>
2019-06-24 17:05:53 +0100

    Error handling implemented
    If an invalid url is passed through it prints the error to the user and appends the error in errorLog.csv

--- BBCArticleGet.py
+++ BBCArticleGet.py
@@ -1,47 +1,70 @@
 import xml.etree.ElementTree as ET  
+from urllib.error import HTTPError
 import urllib.request
 import threading
 import datetime
 import random
 import time
 import csv
+import re
 
 date = datetime.datetime.now()
 currDate = '{}/{}/{}'.format(date.day,date.month,date.year)
 random.seed(datetime.datetime.now())
 
-articleURLs = ('world','uk','business','politics','health',
-'education','science_and_environment','technology','entertainmant_and_arts',
+articleURLs = ('worfld','uk','business','politices','health',
+'education','science_and_environment','technology','entertainment_and_arts',
 'world/africa','world/asia','world/europe','world/latin_america','world/middle_east',
-'world/us_and_canada','world/england','northern_ireland','scotland','wales')
+'world/us_and_canada','england','northern_ireland','scotland','wales')
 
 
 def getArticles(dir):
-    tree = ET.parse(source=urllib.request.urlopen('http://feeds.bbci.co.uk/news/'+dir+'/rss.xml'))
-    root = tree.getroot()
-    invalidArticle = 'BBC News - '+dir
-    allArticles = list()
+    try:
+        tree = ET.parse(source=urllib.request.urlopen('http://feeds.bbci.co.uk/news/'+dir+'/rss.xml'))
+    except HTTPError as err:
+        # print(err)
+        return None
+    except ET.ParseError as err:
+         return None
+    else:
+        root = tree.getroot()
+        allArticles = list()
+        for elem in root.iter('title'):
+            if not re.match('^(BBC News)+', elem.text):
+                    allArticles.append(elem.text)
 
-    for elem in root.iter('title'):
-        if elem.text.lower() != invalidArticle.lower():
-                allArticles.append(elem.text)
-
-    return allArticles
+        return allArticles
 
 
-def writeCSV(articleList, dir):
-    with open('infoXML.csv', 'a', encoding="utf-8") as file:
-        fields = ['date', 'dir', 'articleTitle']
-        writeObj = csv.DictWriter(file, fieldnames=fields,lineterminator='\n')
+def writeCSV(articleList, dir, invalid):
+    if invalid:
+        with open('errorLog.csv', 'a', encoding="utf-8") as file:
+            fields = ['date', 'dir', 'articleTitle']
+            writeObj = csv.DictWriter(file, fieldnames=fields,lineterminator='\n')
 
-        for article in articleList:
-            writeObj.writerow({'date':'{}'.format(currDate),'dir':'{}'.format(dir),'articleTitle':'{}'.format(article)})
+            for article in articleList:
+                writeObj.writerow({'date':'{}'.format(currDate),'dir':'{}'.format(dir),'articleTitle':'{}'.format(article)})
+    else:
+        with open('infoXML.csv', 'a', encoding="utf-8") as file:
+            fields = ['date', 'dir', 'articleTitle']
+            writeObj = csv.DictWriter(file, fieldnames=fields,lineterminator='\n')
+
+            for article in articleList:
+                writeObj.writerow({'date':'{}'.format(currDate),'dir':'{}'.format(dir),'articleTitle':'{}'.format(article)})
 
 
 def scrape(dir):
-    allArticles = getArticles('world')
-    writeCSV(allArticles, dir)
-    print("Downloaded articles from section: {}".format(dir))
+    allArticles = getArticles(dir)
+    if allArticles != None:
+        writeCSV(allArticles, dir, 0)
+        print('Downloaded articles from section: {}'.format(dir))
+    else:
+        badScrapeMsg = 'Error could not scrape from section: {}'.format(dir)
+        badScrape = list()
+        badScrape.append(badScrapeMsg)
+        writeCSV(badScrape, dir, 1)
+        print('############ Failed to download articles from section: {} ############ '.format(dir))
+
 
 
 def main():
