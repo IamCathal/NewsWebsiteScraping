b8b77c33fa57364addcc69913e7a847f5952e3cf
iamcathal <cathalsocallaghan@gmail.com>
2019-11-26 16:53:54 +0000

    Refactored fully into Go

--- articleGetGo.go
+++ articleGetGo.go
@@ -9,25 +9,25 @@ import (
 	"net/http"
 	"os"
 	"regexp"
+	"strings"
 	"sync"
 	"time"
 )
+
+type GuardianItem struct {
+	Data string `xml:",chardata"`
+}
+type GuardianItems struct {
+	Titles   []GuardianItem `xml:"url>news>title"`
+	Keywords []GuardianItem `xml:"url>news>keywords"`
+}
 
 type RTItems struct {
 	XMLName xml.Name `xml:"rss"`
 	RTItems []RTItem `xml:"channel>item"`
 }
-
 type RTItem struct {
 	Title string `xml:"title"`
-}
-
-type GUARDIANItems struct {
-	XMLName       xml.Name       `xml:"urlset"`
-	GUARDIANItems []GUARDIANItem `xml:"url"`
-}
-type GUARDIANItem struct {
-	Title string `xml:"loc"`
 }
 
 // For CNN and BBC
@@ -41,19 +41,17 @@ type Item struct {
 	Description string   `xml:"description"`
 }
 
-// RT and Guardian need their own structs
-// and parsing, namespaces involved for Guardian
-
 func getPage(feedUrl string, section string, siteName string) {
+	// GETs the XML from the various sources
 	res, err := http.Get(feedUrl)
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	wholeXML, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		log.Fatal("Error putting file into buffer\n")
 	}
+
 	if siteName == "BBC" {
 		var BBCItemsStruct Items
 		xml.Unmarshal(wholeXML, &BBCItemsStruct)
@@ -70,17 +68,48 @@ func getPage(feedUrl string, section string, siteName string) {
 		var RTItemsStruct RTItems
 		xml.Unmarshal(wholeXML, &RTItemsStruct)
 		writeRTCSV("RT", section, RTItemsStruct)
-		fmt.Printf("[RT] - %s\n", section)
+		fmt.Printf("[RT] %s\n", section)
 
 	} else if siteName == "GUARDIAN" {
-		var GUARDIANItemsStruct GUARDIANItems
-		xml.Unmarshal(wholeXML, &GUARDIANItemsStruct)
-		fmt.Printf("Unmarshalled: %s\n", GUARDIANItemsStruct)
-		writeGUARDIANCSV("GUARDIAN", section, GUARDIANItemsStruct)
+		GuardianItemsStruct := new(GuardianItems)
+		xml.Unmarshal(wholeXML, &GuardianItemsStruct)
+		writeGuardianCSV("guardian", GuardianItemsStruct)
 	}
 }
 
-func writeGUARDIANCSV(siteName string, section string, correctStruct GUARDIANItems) {
+func writeGuardianCSV(siteName string, correctStruct *GuardianItems) {
+	// Writes the Guardian CSV file, different to the others
+	// so a single function cannot be shared
+	writeTo := fmt.Sprintf("%s.csv", siteName)
+	currTime := time.Now()
+	timeString := fmt.Sprintf("%d/%d/%d", currTime.Day(), currTime.Month(), currTime.Year())
+
+	csvFile, err := os.OpenFile(writeTo, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModeAppend)
+	defer csvFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	csvWriter := csv.NewWriter(csvFile)
+	defer csvWriter.Flush()
+	fmt.Printf("[Guardian] %d articles scraped\n", len(correctStruct.Titles))
+
+	for i := 0; i < len(correctStruct.Titles); i++ {
+		currTitle := fmt.Sprintf("%s", correctStruct.Titles[i])
+		currKeyword := fmt.Sprintf("%s", correctStruct.Keywords[i])
+		if len(currKeyword) < 1 {
+			currKeyword = "nothing"
+		}
+		res := []string{timeString, "guardian", strings.Trim(currTitle, "{}\n"), strings.Trim(currKeyword, "{}\n")}
+		err := csvWriter.Write(res)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func writeRTCSV(siteName string, section string, correctStruct RTItems) {
+	// Writes the RT CSV file, again it's not the same as the others
+	// so it must have it's own function
 	dontWrite := false
 	writeTo := fmt.Sprintf("%s.csv", siteName)
 	currTime := time.Now()
@@ -94,35 +123,22 @@ func writeGUARDIANCSV(siteName string, section string, correctStruct GUARDIANIte
 	csvWriter := csv.NewWriter(csvFile)
 	defer csvWriter.Flush()
 
-	for i := 0; i < len(correctStruct.GUARDIANItems); i++ {
-		if len(correctStruct.GUARDIANItems[i].Title) < 1 {
+	for i := 0; i < len(correctStruct.RTItems); i++ {
+		if len(correctStruct.RTItems[i].Title) < 1 {
 			dontWrite = true
 		}
-
 		if !dontWrite {
-			// regexMatch := regexp.MustCompile(`[<](.)+[>]`)
-			// descString := regexMatch.ReplaceAllString(correctStruct[i].Description, "")
-			descString := "eee"
-			if len(descString) < 1 {
-				res := []string{timeString, section, correctStruct.GUARDIANItems[i].Title, "nothing"}
-				err := csvWriter.Write(res)
-
-				if err != nil {
-					log.Fatal("Can't write to file", err)
-				}
-			} else {
-				res := []string{timeString, section, correctStruct.GUARDIANItems[i].Title, descString}
-				err := csvWriter.Write(res)
-				if err != nil {
-					log.Fatal("Can't write to file", err)
-				}
-
+			res := []string{timeString, section, correctStruct.RTItems[i].Title}
+			err := csvWriter.Write(res)
+			if err != nil {
+				log.Fatal("Can't write to file", err)
 			}
 		}
 	}
 }
 
 func writeCSV(siteName string, section string, correctStruct Items) {
+	// Writes the CSV files for both CNN and BBC
 	dontWrite := false
 	writeTo := fmt.Sprintf("%s.csv", siteName)
 	currTime := time.Now()
@@ -142,59 +158,18 @@ func writeCSV(siteName string, section string, correctStruct Items) {
 		}
 
 		if !dontWrite {
+			// Cleans descriptions of all html tags and attributes7
+			// Isn't the best way I know
 			regexMatch := regexp.MustCompile(`[<](.)+[>]`)
 			descString := regexMatch.ReplaceAllString(correctStruct.Items[i].Description, "")
 			if len(descString) < 1 {
 				res := []string{timeString, section, correctStruct.Items[i].Title, "nothing"}
 				err := csvWriter.Write(res)
-
 				if err != nil {
 					log.Fatal("Can't write to file", err)
 				}
 			} else {
 				res := []string{timeString, section, correctStruct.Items[i].Title, descString}
-				err := csvWriter.Write(res)
-				if err != nil {
-					log.Fatal("Can't write to file", err)
-				}
-
-			}
-		}
-	}
-}
-
-func writeRTCSV(siteName string, section string, correctStruct RTItems) {
-	dontWrite := false
-	writeTo := fmt.Sprintf("%s.csv", siteName)
-	currTime := time.Now()
-	timeString := fmt.Sprintf("%d/%d/%d", currTime.Day(), currTime.Month(), currTime.Year())
-
-	csvFile, err := os.OpenFile(writeTo, os.O_APPEND|os.O_CREATE|os.O_WRONLY, os.ModeAppend)
-	defer csvFile.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-	csvWriter := csv.NewWriter(csvFile)
-	defer csvWriter.Flush()
-
-	for i := 0; i < len(correctStruct.RTItems); i++ {
-		if len(correctStruct.RTItems[i].Title) < 1 {
-			dontWrite = true
-		}
-
-		if !dontWrite {
-			// regexMatch := regexp.MustCompile(`[<](.)+[>]`)
-			// descString := regexMatch.ReplaceAllString(correctStruct.RTItems[i].Description, "")
-			descString := "e"
-			if len(descString) < 1 {
-				res := []string{timeString, section, correctStruct.RTItems[i].Title, descString}
-				err := csvWriter.Write(res)
-
-				if err != nil {
-					log.Fatal("Can't write to file", err)
-				}
-			} else {
-				res := []string{timeString, section, correctStruct.RTItems[i].Title, "e"}
 				err := csvWriter.Write(res)
 				if err != nil {
 					log.Fatal("Can't write to file", err)
@@ -231,13 +206,11 @@ func scrapeCNN(id int, waitG *sync.WaitGroup) {
 	waitG.Done()
 }
 
-// func scrapeGuardian(id int, waitG *sync.WaitGroup) {
-func scrapeGuardian() {
-
+func scrapeGuardian(id int, waitG *sync.WaitGroup) {
 	currUrl := "https://www.theguardian.com/sitemaps/news.xml"
 	getPage(currUrl, "guardian", "GUARDIAN")
 
-	// waitG.Done()
+	waitG.Done()
 }
 
 func scrapeRT(id int, waitG *sync.WaitGroup) {
@@ -251,18 +224,20 @@ func scrapeRT(id int, waitG *sync.WaitGroup) {
 }
 
 func main() {
+	// Uses go routines to achieve
+	// faster scraping times overall
 	var waitG sync.WaitGroup
 	waitG.Add(1)
 	go scrapeBBC(1, &waitG)
 	waitG.Add(1)
 	go scrapeCNN(1, &waitG)
+	waitG.Add(1)
+	scrapeGuardian(1, &waitG)
+	waitG.Add(1)
+	scrapeRT(1, &waitG)
 
-	// scrapeGuardian()
-
-	// waitG.Add(1)
-	// go scrapeRT(1, &waitG)
+	// Once all go routine are completed, continue
 	waitG.Wait()
-	fmt.Printf("\nDone!\n")
 
-	// getPage("https://iamcathal.github.io")
+	fmt.Printf("Done!\n")
 }
